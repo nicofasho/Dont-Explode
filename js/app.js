49963c09979def6a26d45f9d48df537b321a7074
nicofasho <38410585+nicofasho@users.noreply.github.com>
2019-05-04 08:17:52 -0700

    reveal function started

--- js/app.js
+++ js/app.js
@@ -3,13 +3,12 @@ const STATE = {
   bomb: 1,
   blank: 0,
   flag: "F",
-  "question mark": "Q",
-}
+  "question mark": "Q"
+};
 
 /*----- app's state (variables) -----*/
 var initialCell,
-  visibleBoard,
-  hiddenBoard,
+  board,
   boardWidth,
   boardHeight,
   exploded,
@@ -25,51 +24,74 @@ var initialCell,
 /*----- functions -----*/
 init();
 
-function handleClick(evt){
+function handleClick(evt) {
   //fetch value of clicked cell
 
   //cells should be id'd 'i-j' in nested for loops that generate them in the html
-  let cellId = evt.target.id.split('-');
+  let cellId = evt.target.id.split("-");
   let colIdx = cellId[0];
   let rowIdx = cellId[1];
-  let hiddenCell = hiddenBoard[cellId[0]][cellId[1]];
-  let visibleCell = visibleBoard[cellId[0]][cellId[1]]; 
+  let cell = board[cellId[0]][cellId[1]];
   exploded = hiddenCell.value;
-    //if 0
-    if(exploded === 0){
-      //if around === 0 then reveal all 8
-        //call function on that cell, make it recursive somehow.... somehow
-      //if around > 0 then only reveal self
+  //if 0
+  if (exploded === 0) {
+    //if around === 0 then reveal all 8
+    //call function on that cell, make it recursive somehow.... somehow
+    //if around > 0 then only reveal self
+    reveal(cell, colIdx, rowIdx);
+  }
+  //repeat recursively
+  //AND around === 0, then reveal all 8 squares around it
+  //check if all 8 cells exist first
+  //then call said function, oh boi
+  //otherwise just reveal self
+}
 
-      
-      if(hiddenCell.around === 0){
-        
+function render() {}
+
+function reveal(cell, colIdx, rowIdx) {
+  if (cell.around === 0) {
+    if(colIdx > 0){
+      if(rowIdx > 0){
+        cell.revealed = true;
+        return reveal(board[colIdx - 1][rowIdx - 1],(colIdx - 1),(rowIdx - 1));
       }
+      if(rowIdx < boardWidth - 1){
+        cell.revealed = true;
+        return reveal(board[colIdx - 1][rowIdx + 1],(colIdx - 1),(rowIdx + 1));
+      }
+      cell.revealed = true;
+      return reveal(board[colIdx - 1][rowIdx],(colIdx - 1),rowIdx);
     }
-      //repeat recursively
-      //AND around === 0, then reveal all 8 squares around it
-        //check if all 8 cells exist first
-        //then call said function, oh boi
-      //otherwise just reveal self 
-}
-
-function render() {
-
-}
-
-
-function reveal(cell){
-  if(cell.around === 0){
-
+    if(colIdx < boardHeight - 1){
+      if(rowIdx > 0){
+        cell.revealed = true;
+        return reveal(board[colIdx + 1][rowIdx - 1],(colIdx + 1),(rowIdx - 1));
+      }
+      if(rowIdx < boardWidth - 1){
+        cell.revealed = true;
+        return reveal(board[colIdx + 1][rowIdx + 1],(colIdx + 1),(rowIdx + 1));
+      }
+      cell.revealed = true;
+      return reveal(board[colIdx + 1][rowIdx],(colIdx + 1),rowIdx);
+    }
+    if(rowIdx > 0){
+      cell.revealed = true;
+      return reveal(board[colIdx][rowIdx - 1],colIdx,(rowIdx  - 1));
+    }
+    if(rowIdx < boardWidth - 1){
+      cell.revealed = true;
+      return reveal(board[colIdx][rowIdx + 1],colIdx,(rowIdx + 1));
+    }
+  } else {
+    cell.revealed = true;
   }
 }
-
 
 function init() {
   boardWidth = 10; //TODO: update with dynamic sizing
   boardHeight = 10;
-  visibleBoard = [];
-  hiddenBoard = [];
+  board = [];
   difficulty = "";
   numBombs = 10;
   //TODO: will be calculated from difficulty or input from user
@@ -87,21 +109,21 @@ function init() {
 
 function showBoards() {
   console.log(`Checking if around values are updated : `);
-  let bombImage = '';
+  let bombImage = "";
 
-  hiddenBoard.forEach(function (array, colIdx) {
-    array.forEach(function (element, rowIdx) {
-      bombImage += `${element.value } `;
+  board.forEach(function(array, colIdx) {
+    array.forEach(function(element, rowIdx) {
+      bombImage += `${element.value} `;
     });
     bombImage += `\n`;
   });
   console.log(bombImage);
 
-  let aroundImage = '';
+  let aroundImage = "";
 
-  hiddenBoard.forEach(function (array, colIdx) {
-    array.forEach(function (element, rowIdx) {
-      aroundImage += `${element.around } `;
+  board.forEach(function(array, colIdx) {
+    array.forEach(function(element, rowIdx) {
+      aroundImage += `${element.around} `;
     });
     aroundImage += `\n`;
   });
@@ -110,15 +132,12 @@ function showBoards() {
 
 function initBoards(boardWidth, boardHeight) {
   for (let i = 0; i < boardHeight; i++) {
-    visibleBoard[i] = [];
-    hiddenBoard[i] = [];
+    board[i] = [];
     for (let j = 0; j < boardWidth; j++) {
-      visibleBoard[i][j] = {
-        revealed: false
-      };
-      hiddenBoard[i][j] = {
+      board[i][j] = {
         value: 0,
-        around: 0
+        around: 0,
+        revealed: false
       };
     }
   }
@@ -126,20 +145,19 @@ function initBoards(boardWidth, boardHeight) {
 
 function setBombs(numBombs) {
   //Place bombs on hidden board
-  //Use randomizer on size of hiddenBoard to find bomb positions, limited by numBombs
+  //Use randomizer on size of board to find bomb positions, limited by numBombs
 
   for (let i = numBombs; i > 0; i--) {
     bombX = Math.floor(Math.random() * boardWidth);
     bombY = Math.floor(Math.random() * boardHeight);
 
-
-    //TODO: add OR  hiddenBoard[initialCell.x][initialCell.y].value === STATE.bomb
-    while (hiddenBoard[bombX][bombY].value === STATE.bomb) {
+    //TODO: add OR  board[initialCell.x][initialCell.y].value === STATE.bomb
+    while (board[bombX][bombY].value === STATE.bomb) {
       bombX = Math.floor(Math.random() * boardWidth);
       bombY = Math.floor(Math.random() * boardHeight);
     }
 
-    hiddenBoard[bombX][bombY].value = STATE.bomb;
+    board[bombX][bombY].value = STATE.bomb;
     console.log(`bomb values: X - ${bombY} Y - ${bombX}`);
   }
 
@@ -147,8 +165,8 @@ function setBombs(numBombs) {
 }
 
 function setArounds() {
-  hiddenBoard.forEach(function (array, colIdx) {
-    array.forEach(function (element, rowIdx) {
+  board.forEach(function(array, colIdx) {
+    array.forEach(function(element, rowIdx) {
       //if each position around said element exists
       //start with top, go around clockwise i guess
       //TODO: definitely split this into a function, shit's bout to get messy
@@ -156,45 +174,44 @@ function setArounds() {
 
       if (colIdx > 0) {
         if (rowIdx > 0) {
-          if (hiddenBoard[(colIdx - 1)][(rowIdx - 1)].value === STATE.bomb) {
+          if (board[colIdx - 1][rowIdx - 1].value === STATE.bomb) {
             element.around += 1;
           }
         }
-        if (rowIdx < (boardWidth - 1)) {
-          if (hiddenBoard[(colIdx - 1)][(rowIdx + 1)].value === STATE.bomb) {
+        if (rowIdx < boardWidth - 1) {
+          if (board[colIdx - 1][rowIdx + 1].value === STATE.bomb) {
             element.around += 1;
           }
         }
-        if (hiddenBoard[(colIdx - 1)][rowIdx].value === STATE.bomb) {
+        if (board[colIdx - 1][rowIdx].value === STATE.bomb) {
           element.around += 1;
         }
       }
-      if (colIdx < (boardHeight - 1)) {
+      if (colIdx < boardHeight - 1) {
         if (rowIdx > 0) {
-          if (hiddenBoard[(colIdx + 1)][(rowIdx - 1)].value === STATE.bomb) {
+          if (board[colIdx + 1][rowIdx - 1].value === STATE.bomb) {
             element.around += 1;
           }
         }
-        if (rowIdx < (boardWidth - 1)) {
-          if (hiddenBoard[(colIdx + 1)][(rowIdx + 1)].value === STATE.bomb) {
+        if (rowIdx < boardWidth - 1) {
+          if (board[colIdx + 1][rowIdx + 1].value === STATE.bomb) {
             element.around += 1;
           }
         }
-        if (hiddenBoard[(colIdx + 1)][rowIdx].value === STATE.bomb) {
+        if (board[colIdx + 1][rowIdx].value === STATE.bomb) {
           element.around += 1;
         }
       }
       if (rowIdx > 0) {
-        if (hiddenBoard[colIdx][(rowIdx - 1)].value === STATE.bomb) {
+        if (board[colIdx][rowIdx - 1].value === STATE.bomb) {
           element.around += 1;
         }
       }
-      if (rowIdx < (boardWidth - 1)) {
-        if (hiddenBoard[colIdx][(rowIdx + 1)].value === STATE.bomb) {
+      if (rowIdx < boardWidth - 1) {
+        if (board[colIdx][rowIdx + 1].value === STATE.bomb) {
           element.around += 1;
         }
       }
-
     });
   });
 }
